ade3537a637e082936733ed06e08be1c00fcb18f
Kritika Mishra <67709501+Kritikamishra-16@users.noreply.github.com>
2022-01-26 20:16:24 +0530

    Create Question_12.cpp

---
+++ Adobe/Question_12.cpp
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+    
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+        list<int> l;
+        vector<int> v;
+        for(int i=0;i<n;i++)
+        {
+            while(l.size()>0 and l.back()<a[i])
+            {
+                l.pop_back();
+            }
+            l.push_back(a[i]);
+        }
+        for(auto x:l)
+        {
+            v.push_back(x);
+        }
+        return v;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
+  // } Driver Code Ends
